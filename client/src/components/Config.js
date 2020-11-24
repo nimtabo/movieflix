1af70727b8fb7dd7bb823e952cc23e45ffcf6784
nimtabo <nicholasmtabo@yahoo.com>
2020-11-24 18:31:49 +0300

    fix config key

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -2,7 +2,7 @@
 export const USER_SERVER = "/api/users";
 
 export const API_URL = "https://api.themoviedb.org/3/";
-export const API_KEY = process.env.API_KEY;
+export const API_KEY = "844dba0bfd8f3a4f3799f6130ef9e335";
 
 export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
 
